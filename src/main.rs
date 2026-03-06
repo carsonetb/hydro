f43a9598c0567742f143b63619f65bdaaa69dc15
carsonetb <blackhole243243@gmail.com>
2026-03-06 12:19:10 -0800

    Rust and shiz

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::error::Error;
+use std::{error::Error, thread::Builder};
 
-use inkwell::{OptimizationLevel, context::Context, targets::{InitializationConfig, Target}, types::FunctionType};
+use inkwell::{OptimizationLevel, context::Context, module::Module, targets::{CodeModel, InitializationConfig, RelocMode, Target, TargetMachine}, types::FunctionType};
+
+struct LLVMContext<'ctx> {
+    context: &'ctx Context,
+    module: Module<'ctx>,
+    builder: Builder,
+    machine: TargetMachine,
+}
+
+impl<'ctx> LLVMContext<'ctx> {
+
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     Target::initialize_native(&InitializationConfig::default()).expect("Failed to initialize native target.");
@@ -8,7 +19,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let context = Context::create();
     let module = context.create_module("module");
     let builder = context.create_builder();
-    let execution_engine = module.create_execution_engine()?;
+
+    let triple = TargetMachine::get_default_triple();
+    let target = Target::from_triple(&triple).expect("Unknown target.");
+    let machine = target.create_target_machine(
+        &triple,
+        "generic",
+        "",
+        OptimizationLevel::None,
+        RelocMode::Default,
+        CodeModel::Default,
+    ).unwrap();
 
     let int_type = context.i32_type();
     let main_type = int_type.fn_type(&[], false);
